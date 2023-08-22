3c1ab2a33d6b39258657b71b2ed22538bfa3c540
CaptGuto <tadenafi@gmail.com>
2023-08-22 19:14:25 +0300

    project commit

--- app/src/main/java/com/example/tasker_10/MainActivity.kt
+++ app/src/main/java/com/example/tasker_10/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.example.tasker_10
 
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
+    lateinit var sharedpreference: SharedPreferences
+    lateinit var sharedPrefereceEditor: SharedPreferences.Editor
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sharedpreference = getSharedPreferences("LogInfo", MODE_PRIVATE)
+        sharedPrefereceEditor = sharedpreference.edit()
+
+        var isLoggedIn = sharedpreference.getBoolean("islogged", false)
+
+        if(isLoggedIn){
+            intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }else{
+            intent = Intent(this, MainPage::class.java)
+            startActivity(intent)
+        }
+
     }
-    //this is not a code
+
 }
